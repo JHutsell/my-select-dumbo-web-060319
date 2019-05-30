dc21c5093dcaa7cdc9586bf28fc08f1ac453b38d
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 05:33:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,13 @@
 def my_select(collection)
  # your code here!
- return false if collection.empty?
+ return nil if collection.empty?
  
+ selected = []
+ i = 0 
+ while i < collection.length 
+  selected << collection[i] if yield(collection[i])
+  i += 1
+ end
  
+ selected
 end
